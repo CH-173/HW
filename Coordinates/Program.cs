a1af6df4cbe1e1339a00d2ed15a55bc5deb1be29
CH-173 <55220675+CH-173@users.noreply.github.com>
2021-04-12 13:36:19 +0700

    Add files via upload

---
+++ Coordinates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0, step;
+            double distance;
+            for (step = 0; step < 10; step++)
+            {
+                Console.WriteLine("Введите координату (С / Ю / З / В): ");
+                char way = char.Parse(Console.ReadLine());
+                switch (way)
+                {
+                    case 'С':
+                    case 'с':
+                        y++;
+                        break;
+                    case 'Ю':
+                    case 'ю':
+                        y--;
+                        break;
+                    case 'З':
+                    case 'з':
+                        x--;
+                        break;
+                    case 'В':
+                    case 'в':
+                        x++;
+                        break;
+                }
+            }
+            
+            distance = Math.Sqrt(x * x + y * y);
+            Console.WriteLine("Расстояние от начала координат до точки составляет " + distance + " метров.");
+            Console.ReadKey();
+        }
+    }
+}
